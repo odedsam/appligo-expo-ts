b9a6e49069126376262c56a8872ace1e057df257
fragged-up <154810298+fragged-up@users.noreply.github.com>
2025-06-14 10:35:27 +0300

    init

---
+++ store/useAuth.ts
@@ -0,0 +1,13 @@
+import { create } from 'zustand'
+
+type AuthStore = {
+  user: string | null
+  login: (u: string) => void
+  logout: () => void
+}
+
+export const useAuth = create<AuthStore>((set) => ({
+  user: null,
+  login: (u) => set({ user: u }),
+  logout: () => set({ user: null }),
+}))
